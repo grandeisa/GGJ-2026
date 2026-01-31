1581ba4dfb16336e24ec476f32c4e3d1432a3461
IsaelGabriel <isaelgsfaria@gmail.com>
2026-01-31 15:25:29 -0300

    Make items pickable and throwable by player

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,6 +7,8 @@ const SPEED = 10.0
 @export var bash_duration = 1.0
 
 @onready var input: PlayerInput = $PlayerInput
+@onready var item_area: Area3D = $ItemArea3D
+@onready var item_origin: Node3D = $ItemOrigin
 
 var direction: Vector2 = Vector2.UP
 
--- scripts/state-machine/player/walk_player_state.gd
+++ scripts/state-machine/player/walk_player_state.gd
@@ -3,12 +3,30 @@ extends PlayerState
 
 const TURN_SPEED = 150.0
 
+@export var throw_force : Vector3 = Vector3(0, 200, 400)
+
 var player_rotation = 0
 
+var item: Throwable
 
 func _update_state(_delta: float) -> void:
 	if player.input.is_action_just_pressed("bash"):
 		state_transition.emit(self, "bash")
+	
+	if player.input.is_action_just_pressed("item"):
+		if not item:
+			for body in player.item_area.get_overlapping_bodies():
+				if body is Throwable:
+					item = body
+					body.get_parent().remove_child(item)
+					break
+		else:
+			item.global_position = player.item_origin.global_position
+			item.rotation = player.item_origin.global_rotation
+			get_tree().current_scene.add_child(item)
+			item.apply_force(throw_force.rotated(Vector3.UP, player.rotation.y))
+			
+			item = null
 
 func _physics_update_state(delta: float) -> void:
 	var move_direction: Vector2 = player.input.get_vector("left", "right", "up", "down")
