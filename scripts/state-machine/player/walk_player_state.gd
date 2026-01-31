633b83ace55709745868663ca36746abd6a59756
IsaelGabriel <isaelgsfaria@gmail.com>
2026-01-31 04:06:39 -0300

    Make players turn on move

--- scripts/state-machine/player/walk_player_state.gd
+++ scripts/state-machine/player/walk_player_state.gd
@@ -1,6 +1,11 @@
 extends PlayerState
 ### WALK
 
+const TURN_SPEED = 15.0
+
+var player_rotation = 0
+
+
 func _update_state(_delta: float) -> void:
 	if player.input.is_action_just_pressed("bash"):
 		state_transition.emit(self, "bash")
@@ -13,11 +18,15 @@ func _physics_update_state(delta: float) -> void:
 			move_direction = move_direction.normalized()
 		if move_direction.is_normalized():
 			player.direction = move_direction
-			player.look_at(player.position - Vector3(move_direction.x, 0, move_direction.y))
+		var angle_to_move_direction = Vector2.UP.angle_to(Vector2(move_direction.x, -move_direction.y))
+		#var dir = Vector3.ZERO.look_at(Vector3(move_direction.x,0,move_direction.y))
+		player_rotation = lerp_angle(player_rotation, angle_to_move_direction, TURN_SPEED * delta)
 		var move_velocity = move_direction * Player.SPEED
 		player.velocity = Vector3(move_velocity.x, player.velocity.y, move_velocity.y)
 	else:
 		player.velocity.x = move_toward(player.velocity.x, 0, Player.SPEED)
 		player.velocity.z = move_toward(player.velocity.z, 0, Player.SPEED)
+		
+	player.rotation.y = player_rotation
 
 	player.move_and_slide()
